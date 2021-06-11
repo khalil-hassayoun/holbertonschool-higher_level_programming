23d2cbd2a721a18a1d2bd7c0d887777332ff1483
khalil-hassayoun <70943561+khalil-hassayoun@users.noreply.github.com>
2021-06-11 12:22:26 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-// reads a file
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (error, data) => {
-  console.log(error || data);
+const FileName = process.argv[2];
+fs.readFile(FileName, 'utf8', function (err, line) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(line);
+  }
 });
